bdce2deb90c80a0a35df57edac02e8badd8ca9a7
iuliKB <iulianburduniuc03@gmail.com>
2025-01-13 22:32:49 +0200

    Read image and display as grayscale

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+// opencv-test.cpp
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using std::cout;
+using std::endl;
+using namespace cv;
+
+int main(int argc, char **argv)
+{
+    Mat image = imread("../my-image.jpg", IMREAD_GRAYSCALE);
+
+    if (image.empty())
+    {
+        cout << "Not a valid image file" << endl;
+        return -1;
+    }
+
+    namedWindow("Simple Demo", WINDOW_AUTOSIZE);
+    imshow("Simple Demo", image);
+
+    waitKey(0);
+    destroyAllWindows();
+
     return 0;
 }
