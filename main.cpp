a94d3af99f1b48bb49343994c048c2bae07f3b8f
iuliKB <iulianburduniuc03@gmail.com>
2025-01-14 02:40:09 +0200

    Add SIGWINCH signal handler

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-// opencv-test.cpp
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ncurses.h>
+#include <csignal>
 
 using std::cout;
 using std::endl;
 using namespace cv;
 
 char ascii_gradient[16] = {
-    ' ', // Darkest
+    ' ',
     '\'',
     ',',
     '^',
@@ -24,8 +23,7 @@ char ascii_gradient[16] = {
     '&',
     '8',
     '#',
-    '@' // Lightest
-};
+    '@'};
 
 // Maps the pixel values of an image to a new range
 Mat image_pixel_clamp(Mat image, int min, int max)
@@ -55,7 +53,7 @@ void image_to_ascii(Mat image, char *outMatrix)
     }
 }
 
-// Print opencv::image to terminal using ncurses
+// Print OpenCV::image to terminal using Ncurses
 void printImage(Mat image)
 {
     Mat resized_image;
@@ -70,39 +68,60 @@ void printImage(Mat image)
     image_to_ascii(resized_image, ascii_matrix);
 
     // Print the ASCII matrix to the terminal
+    clear(); // Clear the screen to prevent artifacts
     for (int i = 0; i < resized_image.rows; i++)
     {
         for (int j = 0; j < resized_image.cols; j++)
         {
-            printw("%c", ascii_matrix[i * resized_image.cols + j]);
+            mvprintw(i, j, "%c", ascii_matrix[i * resized_image.cols + j]); // Print character at specific location
         }
+    }
+    // printw("Window size %d x %d", max_x, max_y);
+    refresh(); // Refresh the screen to show changes
+}
+
+// Global variables for handling resize
+Mat global_image;
+Mat global_clamped_image;
+
+// Signal handler for window resize
+void handleResize(int sig)
+{
+    // Recalculate and redraw the image
+    if (!global_clamped_image.empty())
+    {
+        printImage(global_clamped_image);
     }
 }
 
 int main(int argc, char **argv)
 {
-    Mat image = imread("../in-image.jpg", IMREAD_GRAYSCALE);
+    // Load the grayscale image
+    global_image = imread("../in-image.jpg", IMREAD_GRAYSCALE);
 
-    if (image.empty())
+    if (global_image.empty())
     {
         cout << "Not a valid image file" << endl;
         return -1;
     }
 
     // Create second image using the grayscale image, clamp the pixel values to the range 0-15
-    Mat clamped_image = image_pixel_clamp(image, 0, 15);
+    global_clamped_image = image_pixel_clamp(global_image, 0, 15);
 
-    // Start ncurses and get the window size
+    // Start Ncurses
     initscr();
+    noecho();        // Disable echoing of user input
+    curs_set(FALSE); // Hide the cursor
 
-    // Wait for user to input 'ENTER' and end ncurses
+    // Set up resize signal handler
+    signal(SIGWINCH, handleResize);
+
+    // Main loop
     for (;;)
     {
-        refresh();
-        // Print the grayscale clamped image to terminal
-        printImage(clamped_image);
+        handleResize(0); // Initial drawing
         char c = getch();
-        if (c == '\n')
+        if (c == '\n') // Exit on ENTER key
         {
             endwin();
             break;
