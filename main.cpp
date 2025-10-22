6eea08369101580cd4a90c0b7e6bf44bb148921f
Radu Chirilov <radulin4a@gmail.com>
2025-10-23 02:52:01 +0300

    add image select

--- main.cpp
+++ main.cpp
@@ -2,10 +2,17 @@
 #include <opencv2/opencv.hpp>
 #include <ncurses.h>
 #include <csignal>
+#include <filesystem>
+#include <vector>
+#include <string>
+#include <algorithm>
 
 using std::cout;
 using std::endl;
+using std::string;
+using std::vector;
 using namespace cv;
+namespace fs = std::filesystem;
 
 char ascii_gradient[16] = {
     ' ',
@@ -80,6 +87,115 @@ void printImage(Mat image)
     refresh(); // Refresh the screen to show changes
 }
 
+// Get list of image files from a directory
+vector<string> getImageFiles(const string &directory)
+{
+    vector<string> imageFiles;
+    vector<string> extensions = {".jpg", ".jpeg", ".png", ".bmp", ".gif", ".tiff", ".tif"};
+
+    try
+    {
+        for (const auto &entry : fs::directory_iterator(directory))
+        {
+            if (entry.is_regular_file())
+            {
+                string filename = entry.path().filename().string();
+                string extension = entry.path().extension().string();
+
+                // Convert extension to lowercase for comparison
+                std::transform(extension.begin(), extension.end(), extension.begin(), ::tolower);
+
+                // Check if the file has an image extension
+                if (std::find(extensions.begin(), extensions.end(), extension) != extensions.end())
+                {
+                    imageFiles.push_back(filename);
+                }
+            }
+        }
+    }
+    catch (const fs::filesystem_error &e)
+    {
+        return imageFiles;
+    }
+
+    // Sort alphabetically
+    std::sort(imageFiles.begin(), imageFiles.end());
+
+    return imageFiles;
+}
+
+// Interactive menu to select an image file
+string selectImageMenu(const vector<string> &imageFiles)
+{
+    if (imageFiles.empty())
+    {
+        return "";
+    }
+
+    int selectedIndex = 0;
+    int key;
+
+    // Enable keypad for arrow keys
+    keypad(stdscr, TRUE);
+
+    while (true)
+    {
+        clear();
+
+        // Print title
+        attron(A_BOLD);
+        mvprintw(1, 2, "Select an image to display:");
+        attroff(A_BOLD);
+
+        mvprintw(2, 2, "Use UP/DOWN arrows to navigate, ENTER to select, Q to quit");
+
+        // Print image list
+        for (size_t i = 0; i < imageFiles.size(); i++)
+        {
+            if (i == selectedIndex)
+            {
+                attron(A_REVERSE); // Highlight selected item
+                mvprintw(4 + i, 4, "> %s", imageFiles[i].c_str());
+                attroff(A_REVERSE);
+            }
+            else
+            {
+                mvprintw(4 + i, 4, "  %s", imageFiles[i].c_str());
+            }
+        }
+
+        refresh();
+
+        // Get user input
+        key = getch();
+
+        switch (key)
+        {
+        case KEY_UP:
+            if (selectedIndex > 0)
+            {
+                selectedIndex--;
+            }
+            break;
+
+        case KEY_DOWN:
+            if (selectedIndex < imageFiles.size() - 1)
+            {
+                selectedIndex++;
+            }
+            break;
+
+        case '\n': // Enter key
+        case KEY_ENTER:
+            return imageFiles[selectedIndex];
+
+        case 'q':
+        case 'Q':
+            return ""; // User quit
+        }
+    }
+}
+
 // Global variables for handling resize
 Mat global_image;
 Mat global_clamped_image;
@@ -100,19 +216,52 @@ void handleResize(int sig)
 
 int main(int argc, char **argv)
 {
+    // Get list of image files from the images directory
+    string imagesDir = "../images";
+    vector<string> imageFiles = getImageFiles(imagesDir);
+
+    if (imageFiles.empty())
+    {
+        cout << "No image files found in " << imagesDir << " directory" << endl;
+        cout << "Please add some image files (.jpg, .png, .bmp, etc.) to the images directory" << endl;
+        return -1;
+    }
+
+    // Start Ncurses for the menu
+    initscr();
+    noecho();        // Disable echoing of user input
+    curs_set(FALSE); // Hide the cursor
+
+    // Show image selection menu
+    string selectedImage = selectImageMenu(imageFiles);
+
+    // Check if user quit
+    if (selectedImage.empty())
+    {
+        endwin();
+        cout << "No image selected. Exiting..." << endl;
+        return 0;
+    }
+
+    // End ncurses temporarily to load the image
+    endwin();
+
+    // Construct full path to selected image
+    string imagePath = imagesDir + "/" + selectedImage;
+
     // Load the grayscale image
-    global_image = imread("../images/in-image.jpg", IMREAD_GRAYSCALE);
+    global_image = imread(imagePath, IMREAD_GRAYSCALE);
 
     if (global_image.empty())
     {
-        cout << "Not a valid image file" << endl;
+        cout << "Error: Could not load image file: " << imagePath << endl;
         return -1;
     }
 
     // Create second image using the grayscale image, clamp the pixel values to the range 0-15
     global_clamped_image = image_pixel_clamp(global_image, 0, 15);
 
-    // Start Ncurses
+    // Restart Ncurses for image display
     initscr();
     noecho();        // Disable echoing of user input
     curs_set(FALSE); // Hide the cursor
